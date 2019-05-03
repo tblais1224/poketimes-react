20227a263852e80503719e6c5f94b020ef3f8d7f
tblais1224 <thomasblais.student@careerdevs.com>
2019-05-03 16:26:06 -0400

    route vid done v25

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 
-const Home = () => {
+const Contact = () => {
     return (
         <div className="App container">
-            <h4 className="center">Home</h4>
+            <h4 className="center">Contact</h4>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate consectetur tempore, sunt veniam sed aliquid laudantium magnam iste? Placeat ipsam, cum nemo sunt hic possimus repellat velit odio aut enim.</p>
         </div>
     )
 }
 
-export default Home;
+export default Contact;
