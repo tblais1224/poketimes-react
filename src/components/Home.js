e1cfc385b08170f6041be3883fd9f7888b5467b3
tblais1224 <thomasblais.student@careerdevs.com>
2019-05-03 22:17:55 -0400

    use axios to fetch api v29

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Axios from 'axios';
 
 
-const Home = () => {
-    return (
-        <div className="App container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate consectetur tempore, sunt veniam sed aliquid laudantium magnam iste? Placeat ipsam, cum nemo sunt hic possimus repellat velit odio aut enim.</p>
-        </div>
-    )
+class Home extends Component {
+    state = {
+        posts: [ ]
+    }
+    componentDidMount(){
+        Axios.get('https://pokeapi.co/api/v2/pokemon')
+        .then((res) => {
+            console.log(res)
+            this.setState({
+                posts: res.data.results.slice(0,25)
+            })
+        })
+    }
+
+    render() {
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return(
+                    <div className="post card" key={post.name}>
+                        <div className="card-content">
+                            <span className="card-title">{post.name}</span>
+                            <p>{post.url}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No posts yet</div>
+        )
+        return (
+            <div className="App container">
+                <h4 className="center">Home</h4>
+                {postList}
+            </div>
+        )
+    }
 }
-
 export default Home;
